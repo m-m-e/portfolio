32091391e5f0b90d949ad49dd5fb56f5c32c0791
Megan Edwards <megmyfedwards@gmail.com>
2019-05-29 20:06:07 +0200

    Update styles for header and add main section

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,20 +5,22 @@ class Header extends React.Component {
   render() {
     return(
       <header className="header">
-        <div className="language-options"></div>
-        <nav className="menu">
-          <ul className="menu__list">
-            <li className="menu__list-item">
-              <a href="#story" className="menu-link">My story</a>
-            </li>
-            <li className="menu__list-item">
-              <a href="#projects" className="menu-link">Projects</a>
-            </li>
-            <li className="menu__list-item">
-              <a href="#contact" className="menu-link">Contact me</a>
-            </li>
-          </ul>
-        </nav>
+        <div className="wrapper header-wrapper">
+          <div className="language-options"></div>
+          <nav className="menu">
+            <ul className="menu__list">
+              <li className="menu__list-item">
+                <a href="#story" className="menu-link">My story</a>
+              </li>
+              <li className="menu__list-item">
+                <a href="#projects" className="menu-link">Projects</a>
+              </li>
+              <li className="menu__list-item">
+                <a href="#contact" className="menu-link">Contact me</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </header>
     );
   }
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import './main.scss';
+import meg from '../../images/MMEprofessional.png';
 
 class Main extends React.Component {
   render() {
     return(
-      <React.Fragment>
-        I'm the main section
-      </React.Fragment>
+      <main className="main-content">
+        <div className="wrapper main-wrapper">
+          <h1 className="title">Megan M Edwards</h1>
+          <img src={meg} alt="Megan Edwards" className="profile-image"/>
+        </div>
+      </main>
     );
   }
 }
