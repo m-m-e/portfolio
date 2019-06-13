a883821692a9f59c151be5f40989b9fa1b619e58
m-m-e <megmyfedwards@gmail.com>
2019-06-13 17:16:07 +0200

    Add contact icons and footer links

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,30 +1,38 @@
 import React from 'react';
+import './contact.scss';
 
 class Contact extends React.Component {
   render(){
     return(
       <section id="contact" className="contact">
         <h2 className="contact__title">Please get in touch!</h2>
-        <div className="contact-email">
-          <img src="" alt="" className="email__icon"/>
-          <p className="email">megmyfedwards@gmail.com</p>
-        </div>
-        <div className="contact-phone">
-          <img src="" alt="" className="phone__icon"/>
-          <p className="phone">(00 34) 644 75 21 07</p>
-        </div>
-        <div className="contact-github">
-          <img src="" alt="" className="github__icon"/>
-          <p className="github">https://github.com/m-m-e</p>
-        </div>
-        <div className="contact-linkedin">
-        <img src="" alt="" className="linkedin__icon"/>
-          <p className="linkedin">https://www.linkedin.com/in/meganmyfanwyedwards/</p>
-        </div>
-        <div className="contact-twitter">
-          <img src="" alt="" className="twitter__icon"/>
-          <p className="twitter">@Miss_MME23</p>
-        </div>
+        <ul className="contact__list">
+          <li className="contact__list-item">
+            <a href="mailto:megmyfedwards@gmail.com" target="_blank" rel="noopener noreferrer" className="contact__link email">
+              <i className="fas fa-envelope email__icon" />
+            </a>
+          </li>
+          <li className="contact__list-item">
+            <a href="tel:00 34 644 75 21 07" target="_blank" rel="noopener noreferrer" className="contact__link phone">
+              <i className="fas fa-phone phone__icon" />
+            </a>
+          </li>
+          <li className="contact__list-item">
+            <a href="https://github.com/m-m-e" target="_blank" rel="noopener noreferrer" className="contact__link github">
+              <i className="fab fa-github-alt github__icon" />
+            </a>
+          </li>
+          <li className="contact__list-item">
+            <a href="https://www.linkedin.com/in/meganmyfanwyedwards/" target="_blank" rel="noopener noreferrer" className="contact__link linkedin">
+              <i className="fab fa-linkedin-in linkedin__icon" />
+            </a>
+          </li>
+          <li className="contact__list-item">
+            <a href="https://twitter.com/Miss_MME23" target="_blank" rel="noopener noreferrer" className="contact__link twitter">
+              <i className="fab fa-twitter twitter__icon" />
+            </a>
+          </li>
+        </ul>
       </section>
     );
   }
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import './footer.scss';
 
 class Footer extends React.Component {
   render() {
     return(
-      <footer className="footer">I'm the footer</footer>
+      <footer className="footer">
+        <div className="wrapper footer-wrapper">
+          <nav className="footer__menu">
+            <ul className="menu__list">
+              <ul className="menu__list-item">
+                <a href="#story" className="menu-link">My story</a>
+              </ul>
+              <ul className="menu__list-item">
+                <a href="#projects" className="menu-link">Projects</a>
+              </ul>
+              <ul className="menu__list-item">
+                <a href="#contact" className="menu-link">Contact me</a>
+              </ul>
+            </ul>
+          </nav>
+        </div>
+      </footer>
     );
   }
 }
