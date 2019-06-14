b993f121008cb72cd7abdb57cd578e555f772069
m-m-e <megmyfedwards@gmail.com>
2019-06-14 19:31:34 +0200

    Add project data and skills icons

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,14 +1,44 @@
 import React from 'react';
 import './projects.scss';
+import {projectData} from '../../data/projectData';
+import css from '../../images/icons/css3_logo.png';
+import git from '../../images/icons/Git_logo.png';
+import html from '../../images/icons/HTML_Logo.png'; 
+import js from '../../images/icons/JavaScript_logo.png'; 
+import npm from '../../images/icons/Npm-logo.png'; 
+import react from '../../images/icons/react_logo.png'; 
+import sass from '../../images/icons/sass-logo.png'; 
 
 class Projects extends React.Component {
   render(){
     return(
       <section id="projects" className="projects">
         <h2 className="projects__title">My projects</h2>
-        <div className="projects__container">
-
+        <h3 className="projects__subtitle">Here are a few of my individual projects. For group projects please see my CV! These were created using...</h3>
+        <div className="skills__icons__box">
+          <img src={css} alt="CSS3" className="skills__icon"/>
+          <img src={git} alt="Git" className="skills__icon"/>
+          <img src={html} alt="HTML5" className="skills__icon"/>
+          <img src={js} alt="JavaScript" className="skills__icon"/>
+          <img src={npm} alt="Npm" className="skills__icon"/>
+          <img src={react} alt="React" className="skills__icon"/>
+          <img src={sass} alt="SASS" className="skills__icon"/>
         </div>
+        <ul className="projects__list">
+          {projectData
+          .filter(item=> item.url !== '' ? item : false)
+          .map(item => {
+            return(
+              <li className="projects__list-item" key={item.id}>
+                <p className="project__name">{item.name}</p>
+                <p className="project__description">{item.description}</p>
+                <p className="project__date">{item.date}</p>
+                <a className="project__link" href={item.url}>{item.url}</a>
+                <a className="project__link-GH" href={item.urlGH}>{item.urlGH}</a>
+              </li> 
+            )
+            })}
+        </ul>
       </section>
   );
   }
---
+++ src/data/projectData.js
@@ -0,0 +1,68 @@
+const projectData = [
+  {
+    id: 1,
+    name: 'Harry Potter Character Search',
+    description: 'Webpage where you can see a list of Harry Potter Characters, filter them and click each one to see more information. Created with React and SASS.',
+    dateCreated: 'June 2019',
+    url: 'http://beta.adalab.es/f-m3-evaluacion-final-m-m-e/#/',
+    urlGH: 'https://github.com/Adalab/f-m3-evaluacion-final-m-m-e'
+  },
+  {
+    id: 2,
+    name: 'Pokemon Character List',
+    description: 'Webpage where you can see a list of Pokemon with information about their types and click on them to add to your favourites list! Created with React.',
+    dateCreated: 'May 2019',
+    url: 'http://beta.adalab.es/f-m3-evaluacion-intermedia-m-m-e/',
+    urlGH: 'https://github.com/Adalab/f-m3-evaluacion-intermedia-m-m-e'
+  },
+  {
+    id: 3,
+    name: 'Pokemon Pairs Game',
+    description: 'Game where you have to find the pairs of pokemon! Click on them to flip them. Created with HTML, SASS and JavaScript.',
+    dateCreated: 'April 2019',
+    url: 'http://beta.adalab.es/f-m2-evaluacion-final-bis-m-m-e/',
+    urlGH: 'https://github.com/Adalab/f-m2-evaluacion-final-bis-m-m-e'
+  },
+  {
+    id: 4,
+    name: 'TV show Search',
+    description: 'Webpage where you can search for TV shows and click on them to add to your favourites list. Created with HTML, SASS and JavaScript.',
+    dateCreated: 'April 2019',
+    url: '',
+    urlGH: 'https://github.com/Adalab/f-m2-evaluacion-final-m-m-e'
+  },
+  {
+    id: 5,
+    name: 'Guess the number Game',
+    description: 'Game where you have to guess the number between 1 and 100! You have as many guesses as you want, but can you beat your score? Created with HTML, SASS and JavaScript.',
+    dateCreated: 'April 2019',
+    url: 'http://beta.adalab.es/f-m2-evaluacion-intermedia-m-m-e/',
+    urlGH: 'https://github.com/Adalab/f-m2-evaluacion-intermedia-m-m-e'
+  },
+  {
+    id: 6,
+    name: 'Design Challenge',
+    description: 'Webpage created to follow a set design, created with pure HTML and SCSS.',
+    dateCreated: 'March 2019',
+    url: 'http://beta.adalab.es/f-m1-evaluacion-final-m-m-e/',
+    urlGH: 'https://github.com/Adalab/f-m1-evaluacion-final-m-m-e'
+  },
+  {
+    id: 7,
+    name: 'Random Animal Picture Generator',
+    description: 'Webpage where you can click on a button (choose between cats or dogs) and it shows you a random picture of your chosen animal! Created with HTML, CSS and JavaScript.',
+    dateCreated: 'May 2019',
+    url: 'https://m-m-e.github.io/random-animal/',
+    urlGH: 'https://github.com/m-m-e/random-animal'
+  },
+  {
+    id: 8,
+    name: 'The Good Monster?',
+    description: 'Monster created with HTML and SASS - hover over it to see a change!',
+    dateCreated: 'May 2019',
+    url: '',
+    urlGH: 'https://github.com/m-m-e/the-good-monster'
+  }
+];
+
+export {projectData};
